1a604a49a980d7a745cecc543c678c963e05e4ff
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-09-04 16:17:56 +0200

    To export a variable or function from a module, you need to add them to module.exports: module.exports.sayHello = sayHello;

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
 console.log(module);
-//Run this line with command prompt with the code: node app.js
-//it will show its own module.
-//Unlike browser applications, variables we define are not added to the
-//"global" object.
-//Every file in a Node application is a module. Node automatically warps the code in each file with an IIFE
-//(Immediately-invoked Function Expression) to create scope.
-//So, variables and functions defined in one file are only scoped to that file and not visible to toher files unless explicitly exported
