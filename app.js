a8d735d466268e8bf5669a44fb50268412c55563
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-09-04 16:29:11 +0200

    To load a module, use the require function. This function returns the module.exports objects exported from the target module: const logger = require('./logger');

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log(module);
+const logger = require('./logger');
+
+logger = 1;     //This line is overwrite 
+
+//console.log(logger);
+logger.log('message');  //This will print the 'message' in log('message') out.
+//It can print that out because of the function we write in logger.js
+
+//We shouldnt declade a variable named logger and use it like that.
+//Cause if we use it like that, we may declare a new value for it.
+//So for the best, we will not declade an object named logger, but we declare
+//an CONSTANT object name logger
