2465473a7bbb416f35c3144c2aafeec59ff2bb5d
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-09-04 16:11:39 +0200

    About the module in node. Every file in Node application is a module. Node automatically warps the code in each file with an IIF(Immediately-invoked Function Expression) to create scope. So, viriables and function defined in one file are only scoped to that file and not visible to other files unless explicitly exported

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-//This line will create an undifined.
-//Reason ist, global Object cant access to message variable
-//They only scope to this file
-var message = '';
-console.log(global.message);
+console.log(module);
+//Run this line with command prompt with the code: node app.js
+//it will show its own module.
+//Unlike browser applications, variables we define are not added to the
+//"global" object.
+//Every file in a Node application is a module. Node automatically warps the code in each file with an IIFE
+//(Immediately-invoked Function Expression) to create scope.
+//So, variables and functions defined in one file are only scoped to that file and not visible to toher files unless explicitly exported
