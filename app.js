698c0b670ca99696d86130c5674785590960a991
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-09-06 11:43:32 +0200

    Event arguments

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const EventEmittter = require('events'); //load the event module
 const emitter = new EventEmittter(); //constance of this class
 
 //Register a listener
-emitter.on('messageLogged', function()
-{
-    console.log('Listener called');
+emitter.on('messageLogged', (arg)=>{   //e or eventArg is fine
+    console.log('Listener called', arg);
 });
 //in on-method has 2 arguments. the first is the name of the method, and the second is the callback, that print out the inside function
+//This is a arrow function. Appear from ECMAScript6
 
 //Raise an event
-emitter.emit('messageLogged');
+emitter.emit('messageLogged', {id: 1, url: 'http://'});//id is 1
 //Notice: emit = making a noise, produce = signaling
+
+//Raise: logging(data: message)
