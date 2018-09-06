16cac9338227998c6c5874aed12934541e454233
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-09-06 08:57:34 +0200

    This is Path modules

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-const log = require('./logger');
+const path = require('path');
 
+var pathObj = path.parse(__filename);
 
-//console.log(logger);
-log('message');  //This will print the 'message' in log('message') out.
-//It can print that out because of the function we write in logger.js
-
-//We shouldnt declade a variable named logger and use it like that.
-//Cause if we use it like that, we may declare a new value for it.
-//So for the best, we will not declade an object named logger, but we declare
-//an CONSTANT object name logger
-
-//Some useful module in the document list on nodejs.org
-//File, HTTP, OS, Path, Process, Query Strings, Stream...
+console.log(pathObj);
+//This code show out the directory to that file that we are executing.
