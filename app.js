15c9afae96cd6787699cd03769715d4c668bc429
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-09-06 08:48:27 +0200

    This is how node wrapp our code. Nodejs does not execute our code directly.

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const log = require('./logger');
+var x = ;
+var url = 'http://myblogger.io/log';
 
+function log(message){
+    //Send an HTTP request
+    console.log(message);
+}
 
-//console.log(logger);
-log('message');  //This will print the 'message' in log('message') out.
-//It can print that out because of the function we write in logger.js
-
-//We shouldnt declade a variable named logger and use it like that.
-//Cause if we use it like that, we may declare a new value for it.
-//So for the best, we will not declade an object named logger, but we declare
-//an CONSTANT object name logger
+module.exports = log;
--- logger.js
+++ logger.js
@@ -1,4 +1,7 @@
-var url ='http://mylogger.io/log';
+(function (exports, require, module, __filename, __dirname) { 
+    console.log(_filename);
+    console.log(__dirname);
+    var url ='http://mylogger.io/log';
 
 function log(message){
     //Send an HTTP request
@@ -6,4 +9,18 @@ function log(message){
 }
 
 module.exports = log;
+
+//In runtime, our code will be wrapped into a function like this
+//(function (exports, require, module, __filename, __dirname) { 
+})
+
+//This look like a function in javascript
+//Node has a few built in modules that enable us to work with the file system, path objectsm network, operating system, etc
+//Nodejs does not execute our code directly
+//Required function: appears to be global function, but actually not a global function
+//Module in the function is the module that we use in module.exports
+//You can add a function to module of export's object, you can write
+//module.exports.log = log;
+//exports.log = log;
+//but not write exports = log; because that export is referenced to module.exports
 
