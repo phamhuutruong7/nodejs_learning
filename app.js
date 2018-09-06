0d67a2f69c364b871abde00cf1e6ea889aea159e
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2018-09-06 09:02:58 +0200

    OS Module in nodejs

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const path = require('path');
+const os = require('os');
+//This is how to call a function
 
-var pathObj = path.parse(__filename);
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
 
-console.log(pathObj);
-//This code show out the directory to that file that we are executing.
+//console.log('Total Memory: ' + totalMemory);
+
+//Template string
+//ES6/ ES2015: ECMAScript 6
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
+
+//This is about how JS can work with the OS, not only on the server side. 
+//This example here is how it can read the data of the computer.
